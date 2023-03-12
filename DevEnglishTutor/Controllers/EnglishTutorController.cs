18d657a3817d5404233b82d1ccf6862fce89391f
Pedrolustosa <pedroeternalss@gmail.com>
2023-03-12 19:55:19 -0300

    Add project files.

---
+++ DevEnglishTutor/Controllers/EnglishTutorController.cs
@@ -0,0 +1,41 @@
+﻿using DevEnglishTutor.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+namespace DevEnglishTutor.Controllers
+{
+    [ApiController]
+    [Route("api/english-tutor")]
+    public class EnglishTutorController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        public EnglishTutorController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string text, [FromServices] IConfiguration configuration)
+        {
+            var token = configuration.GetValue<string>("ChatGPT");
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var model = new ChatGPTInputModel(text);
+
+            var requestBody = JsonSerializer.Serialize(model);
+
+            var content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("https://api.openai.com/v1/completions", content);
+
+            var result = await response.Content.ReadFromJsonAsync<ChatGPTViewModel>();
+
+            var promptResponse = result?.choices?.FirstOrDefault();
+
+            return Ok(promptResponse?.text?.Replace("\n", "").Replace("\t", ""));
+        }
+    }
+}
---
+++ DevEnglishTutor/Models/ChatGPTInputModel.cs
@@ -0,0 +1,20 @@
+﻿namespace DevEnglishTutor.Models
+{
+    public class ChatGPTInputModel
+    {
+
+        public ChatGPTInputModel(string prompt)
+        {
+            this.prompt = $"Correct this english phrase: {prompt}";
+            temperature = 0.2m;
+            max_tokens = 100;
+            model = "text-davinci-003";
+        }
+
+        public string? model { get; set; }
+        public string? prompt { get; set; }
+        public int max_tokens { get; set; }
+        public decimal temperature { get; set; }
+    }
+
+}
